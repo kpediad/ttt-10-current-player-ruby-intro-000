e2e5f4d0276b2896a16deb6620bbf056aed26c9c
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-21 14:59:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,10 @@
 def turn_count(board)
   count = 0
-  board.each {|place| if place == "X" || place == "O" count += 1}
+  board.each do |place|
+    if place == "X" || place == "O" 
+      count += 1
+    end
+  end
 end
 
 def current_player(board)
